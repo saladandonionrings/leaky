81d6718607c8fdcfc624d9d8af53cdd47329c4b3
salad <61053314+saladandonionrings@users.noreply.github.com>
2023-10-13 15:49:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 apt install python3-mysqldb python3-pip
-pip3 install python-magic bottle passlib beaker pymongo
+pip3 install python-magic bottle passlib beaker pymongo math secrets
 
 db="DBleaks"
 mongo --eval "db.credentials.createIndex({\"d\":\"hashed\"})" "$db" > /dev/null 2>&1
 mongo --eval "db.credentials.createIndex({\"l\":\"hashed\"})" "$db" > /dev/null 2>&1
 mongo --eval "db.createCollection(\"leaks\")" "$db" > /dev/null 2>&1
 
-python3 db-users.py
+python3 init.py
